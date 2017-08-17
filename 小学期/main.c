ab434312e080733ef55a7f8ed9310dc0e9ce20a9
kingwangz <540886982@qq.com>
2017-08-17 15:21:56 +0800

    Initial Commit

---
+++ 小学期/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  小学期
+//
+//  Created by kingwang on 2017/8/17.
+//  Copyright © 2017年 kingwang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
