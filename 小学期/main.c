89c27fcb4e51ac0626e076cb9c382d464919475d
kingwangz <540886982@qq.com>
2017-08-18 19:07:11 +0800

    administrator()

--- 小学期/main.c
+++ 小学期/main.c
@@ -23,18 +23,28 @@ int main()
 
 void registered(){
     int i=1;
-    char a;
+    char a,phantom;;
+    char code[10];
     while(i==1)
     {
         printf("(a)Individual user registration\n");
         printf("(b)Administrator registration\n");
         printf("(c)	Exit\n");
         scanf("%c",&a);
+        scanf("%c",&phantom);
         if(a=='a')
         {personal();
         }
         else if(a=='b')
-        {administrator();
+        {   printf("Enter the administrator invitation code\n");
+            scanf("%s",code);
+            scanf("%c",&phantom);
+            if(strcmp(code,"kingwang")==0){
+                administrator();
+            }
+            else{
+                printf("Invitation code error\n");
+            }
         }
         else if(a=='c')
         {exit(1);}
@@ -134,5 +144,95 @@ void personal(){
 
 
 void administrator(){
-    
+    char ID[15],name[15],gender[15],phone[15],password[15],mailbox[15],airline[15];
+    int valid=0;
+    char phantom;
+    while(valid==0){
+        printf("User ID\n");
+        printf("Length 6-10 characters\n");
+        scanf("%s",ID);
+        scanf("%c",&phantom);
+        if(strlen(ID)>5&&strlen(ID)<11){
+            valid=1;
+        }
+        for(int k=0;k<11&&valid==1;k++) {
+            if(ispunct(ID[k])!=0){
+                valid=0;
+            }
+        }
+        if(valid==1&&phantom==' '){
+            valid=0;
+        }
+    }
+    printf("Name\n");
+    scanf("%s",name);
+    while (valid==1) {
+        printf("Gender\n");
+        printf("Please choose a man or a lady\n");
+        scanf("%s",gender);
+        scanf("%c",&phantom);
+        if (strcmp(gender,"man")==0||strcmp(gender,"lady")==0) {
+            valid=0;
+        }
+        if(valid==0&&phantom==' '){
+            valid=1;
+        }
+    }
+    while(valid==0){
+        printf("Phone\n");
+        printf("The telephone number should be 11 digits\n");
+        scanf("%s",phone);
+        scanf("%c",&phantom);
+        valid=1;
+        if(strlen(phone)!=11){
+            valid=0;
+        }
+        for(int k=0;k<11&&valid==1;k++) {
+            if(isdigit(phone[k])==0){
+                valid=0;
+            }
+        }
+        if(valid==1&&phantom==' '){
+            valid=0;
+        }
+    }
+    printf("Password\n");
+    scanf("%s",password);
+    while(valid==1){
+        printf("Mailbox\n");
+        printf("The correct format is ***@***.***\n");
+        scanf("%s",mailbox);
+        scanf("%c",&phantom);
+        valid=0;
+        if(strlen(mailbox)!=11){
+            valid=1;
+        }
+        for(int k=0;k<3&&valid==0;k++) {
+            if(isalpha(mailbox[k])==0){
+                valid=1;
+            }
+        }
+        if(valid==0 && mailbox[3]!='@'){
+            valid=1;
+        }
+        for(int k=4;k<7&&valid==0;k++) {
+            if(isalpha(mailbox[k])==0){
+                valid=1;
+            }
+        }
+        if(mailbox[7]!='.'&&valid==0){
+            valid=1;
+        }
+        for(int k=8;k<11&&valid==0;k++) {
+            if(isalpha(mailbox[k])==0){
+                valid=1;
+            }
+        }
+        if(valid==0&&phantom==' '){
+            valid=1;
+        }
+    }
+    printf("Airline\n");
+    scanf("%s",airline);
+
 }
